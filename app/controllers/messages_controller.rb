1a045013d60b7a081a78fe7ceb6cbcbf5e4e3795
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-01 16:59:34 -0500

    Setup API

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+    def index
+        messages = Message.all
+
+        if messages
+            render json: { data: messages }
+        end
+    end
+
+    def show
+        messages = Message.all
+        message = messages[rand(messages.length - 1)]
+
+        if message
+            render json: { data: message }
+        end
+    end
 end

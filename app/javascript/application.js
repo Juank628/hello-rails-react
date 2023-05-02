419931e5e19c65661bca951be7451cf5ebd60ff8
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-02 11:57:55 -0500

    Configure Redux and fetch data from API

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,15 +5,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Greeting/>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting/>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { readAllGreetings } from "../redux/slices/greetingsSlice";
 
 export default function Greeting() {
+    const greeting = useSelector((store) => store.greetings) 
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(readAllGreetings())
+    },[])
+
     return (
-        <h1>Greetings</h1>
+        <h1>{ greeting.greeting.data?.text || 'Loading...' }</h1>
     )
 }
